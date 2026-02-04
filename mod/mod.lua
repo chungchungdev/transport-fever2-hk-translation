381bd437c94e243c432932375d90149a581c491b
chungchungdev <chungchung.dev@gmail.com>
2026-02-04 20:00:51 +0800

    edit mod

--- mod/mod.lua
+++ mod/mod.lua
@@ -1,15 +1,19 @@
 function data()
   return {
     info = {
-      description = _("MOD_DESC"),
       name = _("MOD_NAME"),
+      description = _("MOD_DESC"),
       tags = { "Asia", "Misc", "chinese", "中文", "繁中", "香港" },
       authors = {
         {
           name = "Chung",
           role = 'CREATOR',
         }
-      }
+      },
+      minorVersion = 1,
+      severityAdd = "NONE",
+      severityRemove = "NONE",
+      url = "https://github.com/chungchungdev/transport-fever2-hk-translation"
     }
   }
 end
