ff8cd3c893bbb25091cb134b07442e540b840fdc
chungchungdev <chungchung.dev@gmail.com>
2026-02-04 20:24:42 +0800

    edit mod tags

--- mod/mod.lua
+++ mod/mod.lua
@@ -3,7 +3,7 @@ function data()
     info = {
       name = _("MOD_NAME"),
       description = _("MOD_DESC"),
-      tags = { "Asia", "Misc", "chinese", "中文", "繁中", "香港" },
+      tags = { "Asia", "Misc", "Chinese", "Localization", "Translation", "中文", "繁體", "香港" },
       authors = {
         {
           name = "Chung",
